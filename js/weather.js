ac38ed0faf5dfaf8f8516d06cb406be554b6ca5d
ueii <kjh2623kjh@gmail.com>
2024-05-13 01:38:13 +0900

    weather

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+function weather_API (lat, lon, key) {
+    return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`
+}
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    fetch(weather_API(lat, lon, WEATHER_KEY))
+        .then(response => response.json())
+        .then(data => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+
+            weather.innerHTML = data.weather[0].main;
+            city.innerHTML = `${data.name} / ${data.main.temp}`;
+        }
+    );
+}
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
